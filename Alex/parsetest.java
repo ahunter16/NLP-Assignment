66c12e0d685073ac480b0df4e33a42591024c313
ahunter16 <alex.hunter14@btinternet.com>
2015-10-28 22:58:39 +0000

    Managing to parse and print outputs
    Just some playing around; managed to get the parser working, and printed
    string and non-string versions of a regular parse tree, as well as a
    flattened one.

--- Alex/parsetest.java
+++ Alex/parsetest.java
@@ -14,6 +14,44 @@ import edu.stanford.nlp.ling.Sentence;
 import edu.stanford.nlp.trees.*;
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
 
-class ParseTest{
+class parsetest {
+	public static void main(String[] args)
+	{
+		String filename = "single.txt";
+		String parserModel = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
+		LexicalizedParser lp = LexicalizedParser.loadModel(parserModel);
 
+		TreebankLanguagePack tlp = lp.treebankLanguagePack(); // a PennTreebankLanguagePack for English
+	    GrammaticalStructureFactory gsf = null;
+	    if (tlp.supportsGrammaticalStructures()) 
+	    {
+	      gsf = tlp.grammaticalStructureFactory();
+	    }
+	    // You could also create a tokenizer here (as below) and pass it
+	    // to DocumentPreprocessor
+	    for (List<HasWord> sentence : new DocumentPreprocessor(filename)) 
+	    {
+			Tree parse = lp.apply(sentence);
+			Tree flattened = parse.flatten();
+
+			System.out.println(flattened.toString());
+			System.out.println();
+			flattened.pennPrint();
+			System.out.println();
+			
+			System.out.println(parse.toString());
+			System.out.println();	
+			parse.pennPrint();
+			System.out.println();
+
+/*			if (gsf != null) 
+			{
+				GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
+				Collection tdl = gs.typedDependenciesCCprocessed();
+				System.out.println(tdl);
+				System.out.println();
+			}*/
+   		}
+    
+	}
 }
