8b8e3abc70c352d1b9729e9128b5b9cb3671b9e2
ahunter16 <alex.hunter14@btinternet.com>
2015-10-28 20:27:19 +0000

    Created some test files
    made a txt file with 2 sentences and another class file with all the
    necessary imports (some are probably redundant)

---
+++ Alex/parsetest.java
@@ -0,0 +1,19 @@
+
+import java.util.Collection;
+import java.util.List;
+import java.io.StringReader;
+
+import edu.stanford.nlp.process.Tokenizer;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+
+class ParseTest{
+
+}
